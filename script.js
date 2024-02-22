fd6fe474844a8b12b72a8a2a7407f90ca7eb7950
AmpereExecutor <160786439+AmpereExecutor@users.noreply.github.com>
2024-02-21 20:53:58 -0500

    d

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("checkButton").addEventListener("click", function() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            if (xmlhttp.status == 200) {
+                if (xmlhttp.responseText.trim() === "Connected") {
+                    alert("CONNECTED");
+                } else {
+                    alert("Error: Connected Packet not retrieved or does not match expected value.");
+                }
+            } else {
+                alert("Error: Unable to retrieve Connected Packet. Status code: " + xmlhttp.status);
+            }
+        }
+    };
+    xmlhttp.open("GET", "https://ampereexecutor.github.io", true);
+    xmlhttp.send();
+});
